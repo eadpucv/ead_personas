3461f5b3839fe31d0f53a51a587ba87bf68210df
Rodrigo Moya <rodrigo@citisent.com>
2014-05-28 18:24:35 -0300

    Mas limpieza

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-	def set_url_for_bar
+	def set_url_for_bar(url)
 		if session[:admin] == "si"
 			admin_list_url
 		else
--- public/javascripts/ead-bar/ead-bar.js
+++ public/javascripts/ead-bar/ead-bar.js
@@ -21,20 +21,10 @@
 			// Identificar Credenciales
 			if (options.id.length > 0 && options.name.length > 0 && options.mail.length > 0) {
 				
-				// Habilitar dropdown
-				$('li.ead-bar-dropdown ul').hide();
-				$('nav li.ead-bar-dropdown').hover(
-				  	function () {
-						$('ul', this).stop().slideDown(100);
-				  	},
-				  	function () {
-						$('ul', this).stop().slideUp(100);
-				  	}
-				);	
 
 				// Asigno Textos y Url's
-				$("#ead-bar-dropdown_li").prepend("<a href='#' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>"+ options.name + " " + options.lastname + "</a>");
-				$("#ead-bar-dropdown_li-movil").prepend("<a href='#' id='ead_bar_dropdown-session-movil' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>"+ options.name + " " + options.lastname + "</a>");
+				$("#ead-bar-dropdown_li").prepend("<a href='#' id='ead_bar_dropdown-session' class='usuario_inactive'><img class='flecha-usuario' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-user-arrow.gif'>"+ options.name + " " + options.lastname + "</a>");
+				$("#ead-bar-dropdown_li-movil").prepend("<a href='#' id='ead_bar_dropdown-session-movil' class='usuario_inactive'><img class='flecha-usuario' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-user-arrow.gif'>"+ options.name + " " + options.lastname + "</a>");
 				
 				// Perfil de la Wiki
 				if (options.wikipage.length > 0) {
@@ -68,16 +58,40 @@
 				$("#ead_bar_dropdown-end_session-movil").attr('href',options.site_logout_url);
 				$("#ead_bar_dropdown-end_session").attr('href',options.site_logout_url);
 				
-				// Boton en Movil
-				$("#ead_bar_dropdown-session-movil").click(function() {
-					$("#ead_bar_user_menu-movil").toggle();
+				// Usuario.
+				$("#ead-bar-dropdown_li").click(function(e) {
+					if($("#ead_bar_dropdown_sub_ul").is(':visible')) {
+						$("#ead_bar_dropdown-session").removeClass( "usuario_active" ).addClass( "usuario_inactive" );
+						$("#ead_bar_dropdown_sub_ul").hide();
+					} else {
+						$("#ead_bar_dropdown-session").removeClass( "usuario_inactive" ).addClass( "usuario_active" );
+						$("#ead_bar_dropdown_sub_ul").show();
+						e.stopPropagation();
+					}
 				});
-				
+
+				// Usuario, Menu movil
+				$("#ead-bar-dropdown_li-movil").click(function(e) {
+					if($("#ead_bar_user_menu-movil").is(':visible')) {
+						$("#ead_bar_user_menu-movil").hide();
+					} else {
+						$("#ead_bar_user_menu-movil").show();
+					}
+				});				
+
+				// Salir del menu cuando se hace click en otro punto y el menu esta abierto
+				$(document).click(function(){
+	      		if($("#ead_bar_dropdown_sub_ul").is(':visible')) {
+	          		$("#ead_bar_dropdown_sub_ul").hide();
+						$("#ead_bar_dropdown-session").removeClass( "usuario_active" ).addClass( "usuario_inactive" );
+	      		};
+	        	});	
+
 
 			} else {
 				// Habilitar boton y link para iniciar session
-				$("#ead-bar-dropdown_li").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")
-				$("#ead-bar-dropdown_li-movil").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")				
+				$("#ead-bar-dropdown_li").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='iniciarsesion'><img class='usuario_inactive' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")
+				$("#ead-bar-dropdown_li-movil").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='iniciarsesion'><img class='usuario_active' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")				
 			}
 
 			// Identificar Sitio

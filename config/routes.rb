b26e138f10126ef3a8abce0cb97b27452d00380e
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-03-14 01:08:30 -0300

    Avance configuracion proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #   
+  root :to => "others#index"
+  match 'home_user' => 'user#home_user'
+  match 'admin/list' => 'admin#list'
+  match 'logout' => 'sessions#logout'
+  match 'api/data4wp' => 'user#data_for_wp'
+  match ':controller(/:action(/:id(.:format)))'
 end

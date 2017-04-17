77fe9aea806d07f8b081ee6fecf1d14b7078b95e
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-04-17 17:14:20 -0300

    Fix recaptcha config file format

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-	config.public_key  = '6LcMORkTAAAAANhXH1elcLlkOjNZrspYUihq6Tw2'
-	config.private_key = '6LcMORkTAAAAAE8DJFoDBNbdIAk-bpmXN3bb6tvl'
+	config.site_key  = '6LcMORkTAAAAANhXH1elcLlkOjNZrspYUihq6Tw2'
+	config.secret_key = '6LcMORkTAAAAAE8DJFoDBNbdIAk-bpmXN3bb6tvl'
 end

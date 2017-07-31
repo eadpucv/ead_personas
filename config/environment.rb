0681f1456cd210c3d53e1edf7def91f13cf8e10d
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-07-31 18:46:57 -0400

    Cleanup conf

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,6 @@ require File.expand_path('../application', __FILE__)
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://cas.ead.pucv.cl")
-# CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://190.208.62.202:443/cas/")
-#CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://localhost:4000/cas/")
 
 # Initialize the Rails application.
 Rails.application.initialize!

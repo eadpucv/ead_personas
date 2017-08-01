da870259727d81199686374831e2d9a8da21963c
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-08-01 13:26:31 -0400

    https

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Enlaza la aplicacion con el servidor CAS
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
-CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://cas.ead.pucv.cl")
+CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "https://cas.ead.pucv.cl")
 
 # Initialize the Rails application.
 Rails.application.initialize!

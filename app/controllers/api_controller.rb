4db574dac8f68820f7a75ee23f13d55588d2bc30
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-12-01 03:39:09 -0300

    Fix Get user info

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,16 @@
+class ApiController < ApplicationController
+	before_action :doorkeeper_authorize!
+	respond_to    :json
+
+	# GET /me.json
+	def me
+		respond_with current_resource_owner
+	end
+
+	private
+
+	# Find the user that owns the access token
+	def current_resource_owner
+		User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+	end
+end

d988cfcc04da5c2a66fe09693eedbccb0628adc9
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-12-04 01:04:08 -0300

    check base controller for doorkeep base oauth provider

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,34 +1,20 @@
 class ApiController < ApplicationController
-	# # before_action :doorkeeper_authorize!
-	# respond_to    :json
+	
+	before_action -> { doorkeeper_authorize! :public }, only: :show
+	before_action -> { doorkeeper_authorize! :write }, only: :update
 
-	# # GET /me.json
-	# def me
-	# 	respond_with current_resource_owner
-	# end
+	def show
+		render json: current_resource_owner.as_json(except: :password)
+	end
 
-	# private
+	def update
+		render json: { result: current_resource_owner.touch(:updated_at) }
+	end
 
-	# # Find the user that owns the access token
-	# def current_resource_owner
-	# 	# User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
- #        if doorkeeper_token
- #          @current_user ||= User.find(doorkeeper_token.resource_owner_id)
- #        end
-	# end
+	private
 
-
-
-  before_action :doorkeeper_authorize!
-
-  def show
-    render json: current_resource_owner.as_json(except: :password)
-  end
-
-  private
-
-  def current_resource_owner
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
+	def current_resource_owner
+		User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+	end
 
 end

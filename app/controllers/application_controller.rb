96e989ab7f893039800d7de2a41613a0a14c1edd
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-12-13 11:28:41 -0300

    Cambio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -104,7 +104,7 @@ class ApplicationController < ActionController::Base
 	end
 	
 	def set_raven_context
-		Raven.user_context(id: session[:user]["id"])
+		Raven.user_context(id: session[:current_user_id])
 		Raven.extra_context(params: params.to_unsafe_h, url: request.url)
 	end
 

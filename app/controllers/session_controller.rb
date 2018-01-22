4d9da46c2f46dec0379d2745d92209f58bda8e96
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2018-01-22 00:40:11 -0300

    test

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -21,7 +21,7 @@ class SessionController < ApplicationController
 				cookies.signed[:user_id] = { value: result[:user].id.to_s, expires: 2.weeks.from_now }
 			end
 			if params.has_key?(:redirect_uri)
-				redirect_to params[:redirect_uri]#, params: request.parameters
+				redirect_to params[:redirect_uri], params: request.parameters
 				# redirect_to new_user_path(request.parameters)
 			else
 				redirect_to root_url, :notice => "Sesion iniciada!" # Esto se esta ejecutando cuando me logeo y ya existo.

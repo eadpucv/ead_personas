98017b5420391ab3c5f39f896f9326efa13c122f
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2018-01-22 00:02:28 -0300

    test

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -83,7 +83,7 @@ Doorkeeper.configure do
   # The value can be any string. Use nil to disable this feature. When disabled, clients must provide a valid URL
   # (Similar behaviour: https://developers.google.com/accounts/docs/OAuth2InstalledApp#choosingredirecturi)
   #
-  # native_redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
+  native_redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
 
   # Forces the usage of the HTTPS protocol in non-native redirect uris (enabled
   # by default in non-development environments). OAuth2 delegates security in
